53fe21fe58cb16226ec6b372c27c9fe8bbe2f0ab
s1ddok <s1ddok@iMac-Andrey.local>
2016-02-19 23:18:41 +0300

    delete gitignore + small changes

--- CCProjectGenerator/ViewController.swift
+++ CCProjectGenerator/ViewController.swift
@@ -50,8 +50,6 @@ class ViewController: NSViewController {
                         
                     })
                     
-                    
-                    
                 }
             }
             
