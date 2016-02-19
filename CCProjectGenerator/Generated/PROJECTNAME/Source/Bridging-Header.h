a234c5b676d782f57a4b474145a72b5c5f80f105
s1ddok <siddok@gmail.com>
2016-02-19 23:46:23 +0300

    Generated project compiles on ios for now.
    Can’t say anything else for now

--- CCProjectGenerator/Generated/PROJECTNAME/Source/Bridging-Header.h
+++ CCProjectGenerator/Generated/PROJECTNAME/Source/Bridging-Header.h
@@ -4,13 +4,11 @@
 // Imported explicitly and with relative path.
 // Class would otherwise be available but not autocompleting/syntax-highlighting.
 // Perhaps an Xcode bug?
-#import "libs/cocos2d-iphone/cocos2d/Support/CCColor.h"
-#import "libs/cocos2d-iphone/cocos2d-ui/CCBReader/CCBReader.h"
-#import "libs/cocos2d-iphone/cocos2d-ui/CCControl.h"
-#import "libs/cocos2d-iphone/cocos2d-ui/CCButton.h"
-#import "libs/cocos2d-iphone/cocos2d-ui/CCSlider.h"
-#import "libs/cocos2d-iphone/cocos2d-ui/CCTextField.h"
-#import "libs/cocos2d-iphone/external/ObjectAL/ObjectAL/ObjectAL/ObjectAL.h"
-#import "libs/cocos2d-iphone/external/ObjectAL/ObjectAL/ObjectAL/AudioTrack/OALAudioTrack.h"
-#import "libs/cocos2d-iphone/external/ObjectAL/ObjectAL/ObjectAL/AudioTrack/OALAudioTracks.h"
-#import "libs/cocos2d-iphone/external/ObjectAL/ObjectAL/ObjectAL/Session/OALAudioSession.h"
+#import "libs/cocos2d/Support/CCColor.h"
+#if CC_CCBREADER
+#import "libs/cocos2d-ui/CCBReader/CCBReader.h"
+#endif
+#import "libs/cocos2d-ui/CCControl.h"
+#import "libs/cocos2d-ui/CCButton.h"
+#import "libs/cocos2d-ui/CCSlider.h"
+#import "libs/cocos2d-ui/CCTextField.h"
