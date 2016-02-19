a71c52ddf0c1ff4186ebad76d28cae9a4085ef3b
s1ddok <siddok@gmail.com>
2016-02-19 23:18:41 +0300

    delete gitignore + small changes

--- CCProjectGenerator/AppDelegate.swift
+++ CCProjectGenerator/AppDelegate.swift
@@ -11,17 +11,7 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
-
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        /*
-        
-        // Create directory
-        [[NSFileManager defaultManager] createDirectoryAtPath:fileName withIntermediateDirectories:NO attributes:NULL error:NULL];
-        
-        NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-        NSString *documentsDirectory = [paths objectAtIndex:0];
-*/
         // Insert code here to initialize your application
     }
 
