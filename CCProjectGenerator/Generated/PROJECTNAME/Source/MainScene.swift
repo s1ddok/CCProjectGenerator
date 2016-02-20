e2b2576238f176bfa7634a0b74da7eab5bcd833e
s1ddok <siddok@gmail.com>
2016-02-20 14:40:16 +0300

    add swift hello world

--- CCProjectGenerator/Generated/PROJECTNAME/Source/MainScene.swift
+++ CCProjectGenerator/Generated/PROJECTNAME/Source/MainScene.swift
@@ -1,5 +1,20 @@
 import Foundation
 
 class MainScene: CCScene {
+    override init!() {
+        super.init()
+        
+        // Background
+        let sprite = CCSprite(imageNamed:"ic_launcher.png")
+        sprite.position = ccp(0.5, 0.5)
+        sprite.positionType = CCPositionTypeNormalized
+        self.addChild(sprite)
+        
+        // The standard Hello World text
+        let label = CCLabelTTF(string: "Hello World", fontName: "ArialMT", fontSize: 16)
+        label.positionType = CCPositionTypeNormalized
+        label.position = ccp(0.5, 0.25)
+        self.addChild(label)
+    }
 
 }
